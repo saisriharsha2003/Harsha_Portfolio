c2c4ba3a2b2609c3a2baca37faf789af192b7ca8
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2024-11-25 14:59:38 +0530

    fixed some issues in skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,51 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-import { skills } from '../../data/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { skills } from '../../data/constants';
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-position: relative;
-z-index: 1;
-align-items: center;
-
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+`;
 
 const Wrapper = styled.div`
-position: relative;
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-direction: column;
-width: 100%;
-max-width: 1100px;
-gap: 12px;
-@media (max-width: 960px) {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
     flex-direction: column;
-}
-`
+  }
+`;
 
 export const Title = styled.div`
-font-size: 42px;
-text-align: center;
-font-weight: 600;
-margin-top: 20px;
+  font-size: 42px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
   color: ${({ theme }) => theme.text_primary};
   @media (max-width: 768px) {
-margin-top: 12px;
-      font-size: 32px;
+    margin-top: 12px;
+    font-size: 32px;
   }
 `;
 
 export const Desc = styled.div`
-    font-size: 18px;
-    text-align: center;
-    max-width: 600px;
-    color: ${({ theme }) => theme.text_secondary};
-    @media (max-width: 768px) {
-        font-size: 16px;
-    }
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
 `;
 
 const SkillsContainer = styled.div`
@@ -55,7 +54,7 @@ const SkillsContainer = styled.div`
   margin-top: 30px;
   gap: 30px;
   justify-content: center;
-`
+`;
 
 const Skill = styled.div`
   width: 100%;
@@ -75,8 +74,7 @@ const Skill = styled.div`
   }
   border: 0.1px solid #CCBA78;
   box-shadow: #CCBA78 2px 2px 1px;
-
-`
+`;
 
 const SkillTitle = styled.h2`
   font-size: 28px;
@@ -84,7 +82,7 @@ const SkillTitle = styled.h2`
   color: ${({ theme }) => theme.text_primary};
   margin-bottom: 20px;
   text-align: center;
-`
+`;
 
 const SkillList = styled.div`
   display: flex;
@@ -92,7 +90,7 @@ const SkillList = styled.div`
   flex-wrap: wrap;
   gap: 12px;
   margin-bottom: 20px;
-`
+`;
 
 const SkillItem = styled.div`
   font-size: 17px;
@@ -114,43 +112,37 @@ const SkillItem = styled.div`
     font-size: 14px;
     padding: 6px 12px;
   }
-`
+`;
 
 const SkillImage = styled.img`
   width: 24px;
   height: 24px;
-`
-
+`;
 
 const Skills = () => {
   return (
     <Container id="skills">
       <Wrapper>
         <Title>Skills</Title>
-        <Desc>Here are some of my skills on which I have been working on for the past 4 years.
-        </Desc>
+        <Desc>Here are some of my skills on which I have been working on for the past 4 years.</Desc>
         <SkillsContainer>
-          {skills.map((skill) => (
-            <Skill>
+          {skills.map((skill, skillIndex) => (
+            <Skill key={skillIndex}> {/* Added key here */}
               <SkillTitle>{skill.title}</SkillTitle>
               <SkillList>
-                {skill.skills.map((item) => (
-                  <>
-                  <SkillItem>
-                    <SkillImage src={item.image}/>
+                {skill.skills.map((item, itemIndex) => (
+                  <SkillItem key={itemIndex}> {/* Added key here */}
+                    <SkillImage src={item.image} alt={item.name} />
                     {item.name}
                   </SkillItem>
-                  
-                  </>
                 ))}
               </SkillList>
             </Skill>
           ))}
-
         </SkillsContainer>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
