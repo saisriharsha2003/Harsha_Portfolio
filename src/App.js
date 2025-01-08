156048ea628204dce46e54c2502124617ffeccf9
Sai Sri Harsha Rankela <saisriharsha.r@gmail.com>
2025-01-08 10:20:43 +0000

    added border in the navbar to highlight the active section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
-import { darkTheme, lightTheme } from './utils/Themes.js';
+import { darkTheme, lightTheme } from './themes/Themes.js';
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent';
+import {
+  Nav,
+  NavLink,
+  NavbarContainer,
+  Span,
+  NavLogo,
+  NavItems,
+  GitHubButton,
+  ButtonContainer,
+  MobileIcon,
+  MobileMenu,
+  MobileLink,
+} from './NavbarStyledComponent'; // Import styled components
 import { DiCssdeck } from 'react-icons/di';
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
 import { useTheme } from 'styled-components';
-import { FaSun, FaMoon } from 'react-icons/fa';  
+import { FaSun, FaMoon } from 'react-icons/fa';
 import styled from 'styled-components';
 
 const Navbar = ({ toggleTheme }) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [activeSection, setActiveSection] = React.useState('#about'); // Default active section
   const currentTheme = useTheme();
+
+  const handleNavClick = (section) => {
+    setActiveSection(section); // Update the active section state
+  };
 
   return (
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <a style={{ display: "flex", alignItems: "center", color: "white",  cursor: 'pointer' }}>
+          <a
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+              cursor: 'pointer',
+            }}
+          >
             <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
           </a>
         </NavLogo>
@@ -23,14 +47,46 @@ const Navbar = ({ toggleTheme }) => {
           <FaBars onClick={() => setIsOpen(!isOpen)} />
         </MobileIcon>
         <NavItems>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href='#skills'>Skills</NavLink>
-          <NavLink href='#experience'>Experience</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          <NavLink
+            href="#about"
+            className={activeSection === '#about' ? 'active' : ''}
+            onClick={() => handleNavClick('#about')}
+          >
+            About
+          </NavLink>
+          <NavLink
+            href="#skills"
+            className={activeSection === '#skills' ? 'active' : ''}
+            onClick={() => handleNavClick('#skills')}
+          >
+            Skills
+          </NavLink>
+          <NavLink
+            href="#experience"
+            className={activeSection === '#experience' ? 'active' : ''}
+            onClick={() => handleNavClick('#experience')}
+          >
+            Experience
+          </NavLink>
+          <NavLink
+            href="#projects"
+            className={activeSection === '#projects' ? 'active' : ''}
+            onClick={() => handleNavClick('#projects')}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            href="#education"
+            className={activeSection === '#education' ? 'active' : ''}
+            onClick={() => handleNavClick('#education')}
+          >
+            Education
+          </NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href={Bio.github} target="_blank">
+            Github Profile
+          </GitHubButton>
         </ButtonContainer>
 
         <ButtonContainer>
@@ -43,12 +99,63 @@ const Navbar = ({ toggleTheme }) => {
 
         {isOpen && (
           <MobileMenu isOpen={isOpen}>
-            <MobileLink href="#about" onClick={() => setIsOpen(!isOpen)}>About</MobileLink>
-            <MobileLink href='#skills' onClick={() => setIsOpen(!isOpen)}>Skills</MobileLink>
-            <MobileLink href='#experience' onClick={() => setIsOpen(!isOpen)}>Experience</MobileLink>
-            <MobileLink href='#projects' onClick={() => setIsOpen(!isOpen)}>Projects</MobileLink>
-            <MobileLink href='#education' onClick={() => setIsOpen(!isOpen)}>Education</MobileLink>
-            <GitHubButton style={{ padding: '10px 16px', background: `${currentTheme.primary}`, color: 'white', width: 'max-content' }} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            <MobileLink
+              href="#about"
+              onClick={() => {
+                handleNavClick('#about');
+                setIsOpen(!isOpen);
+              }}
+            >
+              About
+            </MobileLink>
+            <MobileLink
+              href="#skills"
+              onClick={() => {
+                handleNavClick('#skills');
+                setIsOpen(!isOpen);
+              }}
+            >
+              Skills
+            </MobileLink>
+            <MobileLink
+              href="#experience"
+              onClick={() => {
+                handleNavClick('#experience');
+                setIsOpen(!isOpen);
+              }}
+            >
+              Experience
+            </MobileLink>
+            <MobileLink
+              href="#projects"
+              onClick={() => {
+                handleNavClick('#projects');
+                setIsOpen(!isOpen);
+              }}
+            >
+              Projects
+            </MobileLink>
+            <MobileLink
+              href="#education"
+              onClick={() => {
+                handleNavClick('#education');
+                setIsOpen(!isOpen);
+              }}
+            >
+              Education
+            </MobileLink>
+            <GitHubButton
+              style={{
+                padding: '10px 16px',
+                background: `${currentTheme.primary}`,
+                color: 'white',
+                width: 'max-content',
+              }}
+              href={Bio.github}
+              target="_blank"
+            >
+              Github Profile
+            </GitHubButton>
           </MobileMenu>
         )}
       </NavbarContainer>
@@ -63,13 +170,14 @@ const ThemeToggle = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
-  padding: 0;  
+  padding: 0;
 `;
 
 const Circle = styled.div`
-  width: 50px; 
+  width: 50px;
   height: 50px;
-  background-color: ${({ theme }) => (theme.bg === '#ffffff' ? '#fff' : '#fff')};  
+  background-color: ${({ theme }) =>
+    theme.bg === '#ffffff' ? '#fff' : '#fff'};
   border-radius: 50%;
   display: flex;
   justify-content: center;
@@ -79,12 +187,12 @@ const Circle = styled.div`
 
 const SunIcon = styled(FaSun)`
   font-size: 18px;
-  color: #ff7e5f; 
+  color: #ff7e5f;
 `;
 
 const MoonIcon = styled(FaMoon)`
   font-size: 18px;
-  color: #ff7e5f; 
+  color: #ff7e5f;
 `;
 
 export default Navbar;
