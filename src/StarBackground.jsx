374ceb4d2a1bbe9c545d33aaf727932c294f9de7
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-05-19 00:07:26 +0530

    added stars for the background

---
+++ src/StarBackground.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StarBackground.css";
+
+const StarBackground = () => {
+  return (
+    <div className="star-background">
+      <div className="stars stars1"></div>
+      <div className="stars stars2"></div>
+      <div className="stars stars3"></div>
+    </div>
+  );
+};
+
+export default StarBackground;
