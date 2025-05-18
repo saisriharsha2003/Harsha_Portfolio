07373d117eed7f7bb1ea5ed1bafbd607a0179a9f
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-05-19 00:06:36 +0530

    modified some packages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'build',
+    },
+    server: {
+      host: true, 
+      port: 5173, 
     },
     plugins: [react()],
     css: {
