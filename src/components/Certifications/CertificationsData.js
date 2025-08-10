37403669e406f2913ba442a55956bc1dac556b17
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-08-10 21:12:28 +0530

    correct certifications paths

--- src/components/Certifications/CertificationsData.js
+++ src/components/Certifications/CertificationsData.js
@@ -1,50 +1,61 @@
+import aws from "../../images/certifications/AWS_Certified_Cloud_Practitioner.jpg";
+import gcp_cdl from "../../images/certifications/Google_Cloud_Digital_Leader.jpg";
+import python_dsa from "../../images/certifications/Programming_DataStructures_And_Algorithms_Using_Python.jpg";
+import docker from "../../images/certifications/Docker_for_the_Absolute_Beginner.jpg";
+import kubernetes from "../../images/certifications/Kubernetes_for_the_Absolute_Beginners.jpg";
+import java_dsa from "../../images/certifications/Java_Data_Structures_and_Algorithms_Masterclass.jpg";
+import azure_ai from "../../images/certifications/Microsoft_Azure_AI_Fundementals.jpg";
+import azure_fundamentals from "../../images/certifications/Microsoft_Azure_Fundementals.jpg";  
+
 const certifications = [
   {
     title: "AWS Cloud Practitioner",
     organization: "Amazon Web Services",
-    image: "src/images/certifications/AWS_Certified_Cloud_Practitioner.jpg",
+    image: aws,
     link: "https://drive.google.com/file/d/1UYtklUnOXLaGAxsHVMBquQVN7I4lVGvk/view?usp=sharing"
   },
   {
     title: "Google Cloud Digital Leader",
     organization: "Google Cloud",
-    image: "src/images/certifications/Google_Cloud_Digital_Leader.jpg",
+    image: gcp_cdl,
     link: "https://drive.google.com/file/d/1X6oNUmZ6xca1lmQAjuKmY2LRmxmY-jE_/view?usp=sharing"
   },
   {
     title: "Programming Data Structures and Algorithms using Python",
     organization: "NPTEL",
-    image: "src/images/certifications/Programming_DataStructures_And_Algorithms_Using_Python.jpg",
+    image: python_dsa,
     link: "https://drive.google.com/file/d/1kAMnJndj9DGXIcIng-kUoo3gXpl49J9m/view?usp=sharing"
   },
   {
     title: "Docker for the Absolute Beginner - Hands On - DevOps",
     organization: "Udemy",
-    image: "src/images/certifications/Docker_for_the_Absolute_Beginner.jpg",
+    image: docker,
     link: "https://drive.google.com/file/d/1NoVFjqUArc4FCDC75AeyyiF-upYRbcZw/view?usp=drive_link"
   },
   {
     title: "Kubernetes for the Absolute Beginners - Hands-on",
     organization: "Udemy",
-    image: "src/images/certifications/Kubernetes_for_the_Absolute_Beginners.jpg",
+    image: kubernetes,
     link: "https://drive.google.com/file/d/13hYs6ju3KMEc1k0mj5wSrh5V6X4BqPoz/view?usp=drive_link"
   },
   {
     title: "Java Data Structures and Algorithms Masterclass",
     organization: "Udemy",
-    image: "src/images/certifications/Java_Data_Structures_and_Algorithms_Masterclass.jpg",
+    image: java_dsa,
     link: "https://drive.google.com/file/d/1kAMnJndj9DGXIcIng-kUoo3gXpl49J9m/view?usp=sharing"
   },
   {
     title: "Microsoft Azure AI Fundamentals",
     organization: "Microsoft Azure",
-    image: "src/images/certifications/Microsoft_Azure_AI_Fundementals.jpg"
+    image: azure_ai,
+    link: "https://drive.google.com/file/d/1wOTOSZSoyiSoqHkKGYcicA8sFYPgYvOt/view?usp=drive_link"
   },
   {
     title: "Microsoft Azure Fundamentals",
     organization: "Microsoft Azure",
-    image: "src/images/certifications/Microsoft_Azure_Fundementals.jpg"
-  },
+    image: azure_fundamentals,
+    link: "https://drive.google.com/file/d/17pkM4DiUDWv_nw1WBtdawtIPh9qtaT-z/view?usp=drive_link"
+  }
 ];
 
 export default certifications;
